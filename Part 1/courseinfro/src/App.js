1dbd7aa5c07ec5f89574f4decdbdd4bea1a9ced9
TylerS2000 <tylerschulten@gmail.com>
2022-11-18 15:02:33 -0500

    update

---
+++ Part 1/courseinfro/src/App.js
@@ -0,0 +1,34 @@
+import Header from "./Header"
+import Content from "./Content"
+import Total from "./Total"
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+  const parts = course.parts
+  
+  return (
+    <div>
+      <Header Course={course.name}/>
+      <Content parts ={parts}/>
+      <Total parts={parts}/>
+    </div>
+  )
+}
+
+export default App
