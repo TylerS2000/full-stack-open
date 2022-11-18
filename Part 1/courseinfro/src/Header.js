9de91063c99b63316cd5898c752fa6fbf71b4bce
TylerS2000 <tylerschulten@gmail.com>
2022-11-18 15:06:42 -0500

    hi

--- Part 1/courseinfro/src/Header.js
+++
@@ -1,6 +0,0 @@
-export default function Header(props){
-    return(
-    <h1>
-        {props.Course}
-    </h1>)
-}
--- Part 1/courseinfro/src/Total.js
+++
@@ -1,6 +0,0 @@
-export default function Total(props){
-   const parts = props.parts
-    return(
-        <p>Number of Exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-    )
-}
