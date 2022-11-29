265389892d7413a383b9abb52d48dcc5a3de1352
TylerS2000 <tylerschulten@gmail.com>
2022-11-29 17:59:59 -0500

    init

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,20 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Filter'
 import Form from './Form'
 import Numbers from './Numbers'
+import axios from 'axios'
 const App = () => {
+  
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const[newNumber,setNewNumber] =useState('')
   const[filter,setFilter]=useState('')
+
+  useEffect(()=>{axios('http://localhost:3001/persons')
+  .then(response=>console.log(response.json())
+ 
+}
+    ,[persons]
+  )
 function handleTextChange(event){
   setNewName(event.target.value)
 }
+
 function submitName(event){
   event.preventDefault()
   for(let i = 0; i<persons.length; i++){
@@ -23,6 +29,7 @@ function submitName(event){
       return
     }
   }
+
   setPersons(x=>{
     return([...x,{name:newName, number:newNumber}])
   })
