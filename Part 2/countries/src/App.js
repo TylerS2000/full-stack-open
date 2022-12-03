5018de73d48abe4130addaba1e9859629f835848
TylerS2000 <tylerschulten@gmail.com>
2022-12-03 17:19:38 -0500

    init

--- Part 2/countries/src/App.js
+++ Part 2/countries/src/App.js
@@ -5,15 +5,19 @@ import React from 'react';
 
 function App() {
   let [search,setSearch]= React.useState('');
-  let[data,setData]=React.useState('')
+  let[data,setData]=React.useState('');
   React.useEffect(()=>{axios.get(`https://restcountries.com/v2/name/${search}`)
-    .then(response=>setData(response.data))},[search]
+    .then(response=>setData(response.data.map(country=>{return {...country,show:false}})))},[search]
   )
 
   function handleChange(event){
     setSearch(event.target.value)
   }
+function showCountry(selectedCountry){
+setData(data=>{return data.map(country=>{console.log(selectedCountry.name===country.name); return country.name===selectedCountry.name?{...country, show:true}:country}) })
+console.log(data)
 
+}
   function renderData(){
     if (data.length<1){return <p>No Match</p>}
     else if(data.length>10){return <p>too many results</p>}
@@ -28,8 +32,11 @@ function App() {
        <img src={data[0].flag} style={{width:"30%"}}/>
        </div>)
     }
-    return data.map(country=><div>{country.name}</div>)
+    return data.map(country=><div>{country.name}{country.show?<div><img src={country.flag} style={{width:"30%"}}/> <h2>Languages:</h2> <ul>
+    {country.languages.map(language=><li>{language.name}</li>)}
+  </ul></div>:<p></p>}<button onClick={()=>showCountry(country)}>show</button></div>)
   }
+
  let render = renderData()
   return (
   <div>
--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -4,6 +4,7 @@ import Form from './Form'
 import Numbers from './Numbers'
 import axios from 'axios'
 import backend from './backend'
+import './App.css' 
 const App = () => {
   
   const [persons, setPersons] = useState([
@@ -11,6 +12,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const[newNumber,setNewNumber] =useState('')
   const[filter,setFilter]=useState('')
+  const[message, setMessage]=useState('')
 
   useEffect(()=>{
     backend
@@ -29,13 +31,24 @@ function submitName(event){
   for(let i = 0; i<persons.length; i++){
     
     if(persons[i].name === newName){
-      alert(`${newName} is already in your phonebook`)
+      if (window.confirm(`${persons[i].name} is already in your phone book. Would you like to replace their number`)){
+        backend.update(persons[i].id,{...persons[i], number:newNumber})
+        .catch(res=>setMessage(`Name is no longer in phone book`))
+        setMessage(`${persons[i].name}'s number has been modified`);
+        setTimeout(
+          ()=>{setMessage(null)},2000
+        )
+      }
       return
     }
+    
   }
       backend.create( {name:newName,number:newNumber})
       .then(response=>console.log(response))
-      
+      setMessage(`${newName}'s number has been added`);
+        setTimeout(
+          ()=>{setMessage(null)},2000
+        )
       
     
       return
@@ -49,6 +62,7 @@ function handleNumberChange(event){ setNewNumber(event.target.value)}
   return (
     <div>
       <h2>Phonebook</h2>
+      <div className='message'>{message}</div>
       <Filter setFilter={setFilter} filter={filter}/>
       <Form newName={newName} handleTextChange={handleTextChange} newNumber={newNumber} handleNumberChange={handleNumberChange} submitName={submitName}/>
       <h2>Numbers</h2>
